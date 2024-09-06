c4d81ab54564d26d7f0b6826310e3dbe841af3e9
Dev Bheda <bhedadev010@gmail.com>
2024-09-06 19:41:04 +0530

    6-9-24

--- Self_Practice/wow4.py
+++ Self_Practice/wow4.py
@@ -1,12 +1,10 @@
-filename = input("Enter the filename :")
+filename = input("Enter file name :")
 filename = filename + ".txt"
 with open(filename,"r") as file:
     file = file.read()
     lene = file.split()
+    print(lene)
     for i in lene:
-        print(i)
         if(len(i)== 4):
-            lene[i] = "****"
-
-    print(file)
-
+            lene[lene.index(i)] = "****"
+print(lene)
